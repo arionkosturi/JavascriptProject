840845d29dee2b33cf7243e5581639886d2292d7
arkotech <arionkosturi@gmail.com>
2024-07-18 13:02:59 +0200

    changed highlighted display

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {
   mainNewsRightSideElement,
   categoriesElement,
   footerElement,
-  state,
   prevButton,
   nextButton,
   localApi,
@@ -23,6 +22,7 @@ import "./components/Darkmode.js";
 
 let artikull = [];
 let pageN = 0;
+let mainNewsRightSideHTML;
 
 Articles({
   url: `${localApi}/?p=${pageN}`,
@@ -54,53 +54,51 @@ fetch(`${localApi}/top`)
     return response.json();
   })
   .then((data) => {
-    state.articles = data;
-
-    // Gallery
-    for (let i = 0; i <= 5; i++) {
-      artikull.push(state.articles[i]);
-      let mainNewsRightSideHTML = `<div id=${i}
-      class="flex mb-2 lg:mt-0 artikull bg-slate-100 dark:bg-neutral-800 opacity-85 hover:opacity-100 hover:shadow-md">
-      <img src="${artikull[i].imgUrl}" class="w-1/3 hover:cursor-default" alt="" />
-      <p id=${i} class="p-1 dark:text-gray-300 text-sm lg:text-md">
-        ${artikull[i].title}
-      </p>
-    </div>`;
+    data.map((article, index) => {
+      artikull.push(data[index]);
+      mainNewsRightSideHTML = `<div id=${index}
+             class="flex mb-2 lg:mt-0 artikull bg-slate-100 dark:bg-neutral-800 opacity-85 hover:opacity-100 hover:shadow-md">
+             <img src="${article.imgUrl}" class="w-1/3 hover:cursor-default" alt="" />
+             <p id=${index} class="p-1 dark:text-gray-300 text-sm lg:text-md">
+               ${article.title}
+             </p>
+           </div>`;
       mainNewsRightSideElement.insertAdjacentHTML(
         "afterbegin",
         mainNewsRightSideHTML
       );
-
-      mainNewsElement.innerHTML = `<div class="opacity-95 hover:opacity-100 mr-2">
-  <a href="article.html?id=${artikull[0]._id}">
-  <img src="${artikull[0].imgUrl}" alt="" class="opacity-90" />
-      <div
-        class="relative bg-purple-800 bg-opacity-100 lg:bg-opacity-80 hover:bg-opacity-90 lg:-mt-28 py-2 w-full h-28 text-white"
-        <h3 class="p-2 md:text-5xl text-gray">
-         ${artikull[0].title}
-        </h3>
-      </div>
-      </a>
-    </div>`;
-    }
-    mainNewsRightSideElement.addEventListener("click", (e) => {
-      e.preventDefault();
-      let id = 0;
-      id = e.target.getAttribute("id");
-      mainNewsElement.innerHTML = `<div  class="opacity-95 hover:opacity-100 mr-2">
-      <a href="article.html?id=${artikull[id]._id}">
-      <img src="${artikull[id].imgUrl}" alt="" class="opacity-90" />
-      <div 
-        class="relative bg-purple-800 bg-opacity-100 lg:bg-opacity-80 hover:bg-opacity-90 lg:-mt-28 py-2 w-full h-28 text-white">
-        <h3 class="p-1 md:text-xl line-clamp-2 ">
-         ${artikull[id].title}
-        </h3>
-      </div>
-      </a>
-    </div>`;
-      window.scrollTo(0, 0);
     });
+    mainNewsElement.innerHTML = `<div class="opacity-95 hover:opacity-100 mr-2">
+  <a href="article.html?id=/">
+  <img src="${artikull[0].imgUrl}" alt="" class="opacity-90" />
+  <div 
+    class="relative bg-purple-800 bg-opacity-100 lg:bg-opacity-80 hover:bg-opacity-90 lg:-mt-28 py-2 w-full h-28 text-white">
+    <h3 class="p-1 md:text-xl line-clamp-2 ">
+    ${artikull[0].title}
+    </h3>
+  </div>
+  </a>
+</div>`;
+
+    return mainNewsRightSideHTML;
   })
   .catch((error) => {
     console.log(error);
   });
+
+mainNewsRightSideElement.addEventListener("click", (e) => {
+  e.preventDefault();
+  let id = e.target.id || 0;
+  mainNewsElement.innerHTML = `<div class="opacity-95 hover:opacity-100 mr-2">
+        <a href="article.html?id=${artikull[id]._id}">
+        <img src="${artikull[id].imgUrl}" alt="" class="opacity-90" />
+        <div 
+          class="relative bg-purple-800 bg-opacity-100 lg:bg-opacity-80 hover:bg-opacity-90 lg:-mt-28 py-2 w-full h-28 text-white">
+          <h3 class="p-1 md:text-xl line-clamp-2 ">
+           ${artikull[id].title}
+          </h3>
+        </div>
+        </a>
+      </div>`;
+  window.scrollTo(0, 0);
+});
